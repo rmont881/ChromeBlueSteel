d8035040f8d9a1bbd281f4ac053bc4783d0312e1
Rob Montgomery <robert.d.montgomery@gmail.com>
2018-10-06 15:43:11 -0600

    Adding null checks for string replace

--- textreplace.js
+++ textreplace.js
@@ -8,18 +8,20 @@ chrome.storage.sync.get('originalString', function(data) {
     originalString = data.originalString;
     chrome.storage.sync.get('replacementString', function(data) {
         replacementString = data.replacementString;
-        for (var i = 0; i < elements.length; i++) {
-            var element = elements[i];
-        
-            for (var j = 0; j < element.childNodes.length; j++) {
-                var node = element.childNodes[j];
-        
-                if (node.nodeType === 3) {
-                    var text = node.nodeValue;
-                    var replacedText = text.replace(new RegExp(originalString, 'gi'), replacementString);
-        
-                    if (replacedText !== text) {
-                        element.replaceChild(document.createTextNode(replacedText), node);
+        if((originalString || !(0 === originalString.length)) && (replacementString || !(0 === replacementString.length))) {
+            for (var i = 0; i < elements.length; i++) {
+                var element = elements[i];
+            
+                for (var j = 0; j < element.childNodes.length; j++) {
+                    var node = element.childNodes[j];
+            
+                    if (node.nodeType === 3) {
+                        var text = node.nodeValue;
+                        var replacedText = text.replace(new RegExp(originalString, 'gi'), replacementString);
+            
+                        if (replacedText !== text) {
+                            element.replaceChild(document.createTextNode(replacedText), node);
+                        }
                     }
                 }
             }
