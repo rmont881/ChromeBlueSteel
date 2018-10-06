9dad032fce1e04dc1f19a3a6c34b3a5593a470d0
Rob Montgomery <robert.d.montgomery@gmail.com>
2018-10-06 15:05:17 -0600

    Adding text replace options

--- options.js
+++ options.js
@@ -15,3 +15,40 @@ function constructOptions(kButtonColors) {
 }
 
 constructOptions(kButtonColors);
+
+//CSS Inject
+let btnSaveCss = document.getElementById('btnSaveCss');
+let taCss = document.getElementById('css');
+
+chrome.storage.sync.get('css', function(data) {
+    taCss.value = data.css;
+});
+
+btnSaveCss.addEventListener('click', function() {
+    chrome.storage.sync.set({ css: taCss.value }, function() {
+        console.log('css has been updated');
+    });
+});
+
+
+//Text Replace
+let btnSaveStrReplaced = document.getElementById('btnSaveStrReplace');
+let ogText = document.getElementById('ogtext');
+let rpText = document.getElementById('rptext');
+
+chrome.storage.sync.get('originalString', function(data) {
+    ogText.value = data.originalString;
+});
+
+chrome.storage.sync.get('replacementString', function(data) {
+    rpText.value = data.replacementString;
+});
+
+btnSaveStrReplaced.addEventListener('click', function() {
+    chrome.storage.sync.set({ originalString: ogText.value }, function() {
+        console.log('ogText has been updated');
+    });
+    chrome.storage.sync.set({ replacementString: rpText.value }, function() {
+        console.log('rpText has been updated');
+    });
+});
